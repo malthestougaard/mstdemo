66dce6d2010cf2793e35f08791267e1ea70a7ebc
Malthe Kirkhoff Stougaard <malthe@stougaard.eu>
2016-12-20 10:16:22 +0100

    Memcached

---
+++ routes/memcached.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var Memcached = require('memcached');
+
+var router = express.Router();
+
+
+var memcached = new Memcached(localhost:11211, {});
+
+
+router.get('/', function(req, res, next) {
+  memcached.get('foo', function (err, data) {
+    console.log(data);
+    res.send(data);
+  });
+});
+
+router.post('/', function(req, res, next) {
+  memcached.set('foo', , 'Hi from Memcached', function (err) {
+    res.send("Data cached...");
+  });
+});
+
+module.exports = router;
